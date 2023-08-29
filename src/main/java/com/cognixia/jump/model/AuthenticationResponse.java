b42246bed22d129edfd4c7117ca54cfbbb6731c3
nedned1108 <rpmshockwave@gmail.com>
2023-08-29 00:01:03 -0400

    added skeleton for model and controller

---
+++ src/main/java/com/cognixia/jump/model/AuthenticationResponse.java
@@ -0,0 +1,24 @@
+package com.cognixia.jump.model;
+
+import java.io.Serializable;
+
+//response object for when we return the token (JWT) for a user
+public class AuthenticationResponse implements Serializable{
+
+	private static final long serialVersionUID = 1L;
+	
+	private String jwt;
+	
+	public AuthenticationResponse(String jwt) {
+		this.jwt = jwt;
+	}
+
+	public String getJwt() {
+		return jwt;
+	}
+
+	public void setJwt(String jwt) {
+		this.jwt = jwt;
+	}
+
+}
---
+++ src/main/java/com/cognixia/jump/model/User.java
@@ -0,0 +1,12 @@
+package com.cognixia.jump.model;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+
+@Entity
+public class User implements Serializable{
+
+	private static final long serialVersionUID = 1L;
+	
+}
