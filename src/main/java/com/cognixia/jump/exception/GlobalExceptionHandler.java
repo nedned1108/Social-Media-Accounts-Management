e6ffee6f72da3cb45a090b4f1a30fd12e5bf9195
kirstiex <ikirstiew@gmail.com>
2023-08-29 13:12:09 -0500

    Added swagger to GlobalExceptionHandler

--- src/main/java/com/cognixia/jump/exception/GlobalExceptionHandler.java
+++ src/main/java/com/cognixia/jump/exception/GlobalExceptionHandler.java
@@ -2,9 +2,11 @@ package com.cognixia.jump.exception;
 
 import java.util.Date;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.context.request.WebRequest;
 
 // advice the controller about what to do when an exception is thrown
@@ -12,6 +14,7 @@ import org.springframework.web.context.request.WebRequest;
 public class GlobalExceptionHandler {
 
 	@ExceptionHandler(ResourceNotFoundException.class)
+	@ResponseStatus(HttpStatus.NOT_FOUND)
 	public ResponseEntity<?> resourceNotFoundException( ResourceNotFoundException ex, WebRequest request ) {
 		
 		// request.getDescription(false) => details on the request (usually includes the uri path where request was made)
@@ -24,6 +27,7 @@ public class GlobalExceptionHandler {
 	}
 	
 	@ExceptionHandler(SameUserAndPlatformException.class)
+	@ResponseStatus(HttpStatus.CONFLICT)
 	public ResponseEntity<?> sameUser(SameUserAndPlatformException ex, WebRequest request) {
 		ErrorDetails errorDetails = 
 				new ErrorDetails(new Date(), ex.getMessage(), request.getDescription(false));
