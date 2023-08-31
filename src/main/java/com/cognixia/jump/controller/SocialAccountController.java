dcc93b111760ea6e39326adf6761073585c0be5c
kirstiex <ikirstiew@gmail.com>
2023-08-31 12:47:15 -0500

    added responses

--- src/main/java/com/cognixia/jump/controller/SocialAccountController.java
+++ src/main/java/com/cognixia/jump/controller/SocialAccountController.java
@@ -4,7 +4,6 @@ import java.util.List;
 import java.util.Optional;
 
 import javax.validation.Valid;
-import javax.websocket.server.PathParam;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -24,8 +23,9 @@ import com.cognixia.jump.model.SocialAccount;
 import com.cognixia.jump.service.SocialAccountService;
 
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 
-import com.cognixia.jump.repository.SocialAccountRepository;
 
 @RequestMapping("/api")
 @RestController
@@ -35,7 +35,8 @@ public class SocialAccountController {
 	SocialAccountService service;
 
 	@Operation( summary = "Get all the accounts in the social_account table",
-			 description = "Get all accounts"
+			 description = "Get all accounts from the social_account table in the social_management database. "
+			 		+ "Each account grabbed has an id, account_name, platform_name, description, and user"
 			 )
 
 	@GetMapping("/account")
@@ -44,8 +45,14 @@ public class SocialAccountController {
 	}
 
 	@Operation( summary = "Get an account by Id",
-			 description = "Get an account by its Id"
+			 description = "Get an account by its Id from the social_account table in the social_management database."
+			 		+"ach account grabbed has an id, account_name, platform_name, description, and user"
 			 )
+	@ApiResponses(
+			@ApiResponse(responseCode="200",
+					description="Account has been found")
+			)
+	
 	@GetMapping("/account/{id}")
 	public ResponseEntity<?> getAccountsById(@PathVariable int id) throws ResourceNotFoundException {
 
@@ -55,9 +62,13 @@ public class SocialAccountController {
 	}
 
 	@Operation( summary = "Create a new account",
-			 description = "Create a new account"
-			 )
-
+			 description = "Create a new account from the social_account table in the social_management database."
+			 		+ "Each account created has an id, account_name, platform_name, description, and user"
+			 )                   
+	@ApiResponses(
+			@ApiResponse(responseCode="201",
+			description="Account has been created")
+			)
 	// create account throws sameUser exception
 	@PostMapping("/account")
 	public ResponseEntity<SocialAccount> createAccount(@Valid @RequestBody SocialAccount account)
@@ -67,10 +78,17 @@ public class SocialAccountController {
 
 		return ResponseEntity.status(201).body(created);
 	}
-
+	
 	@Operation( summary = "Update an account",
-			 description = "Update an account"
-			 )
+	 description = "Update an account from the social_account table in the social_management database."
+	 		+ "Each account updated has an id, account_name, platform_name, description, and user"
+	 )
+			
+	@ApiResponses(
+			@ApiResponse(responseCode="200",
+					description="Account has been updated")
+			)
+	
 	// create account throws sameUser exception
 	@PutMapping("/account")
 	public ResponseEntity<SocialAccount> updateAccount(@Valid @RequestBody SocialAccount account)
@@ -87,8 +105,14 @@ public class SocialAccountController {
 	}
 
 	@Operation( summary = "Delete an account by Id",
-			 description = "Deletes an account by its Id "	 
+			 description = "Deletes an account by its Id from the social_account table in the social_management database."
+			 		+ "Each account grabbed has an id, account_name, platform_name, description, and user"	 
 			 )
+	@ApiResponses(
+			@ApiResponse(responseCode="200",
+					description="Account has been deleted")
+			)
+	
 	@DeleteMapping("/account/{id}")
 	public ResponseEntity<?> deleteAccount(@PathVariable int id) throws ResourceNotFoundException {
 
